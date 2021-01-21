ec164867ee8afb0d7e8b957621d67a4f6b339eb0
Luk1Pr0 <lpytel16@gmail.com>
2021-01-21 14:30:36 +0100

    Created routes for contacts, users and authentication

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 app.get('/', (req, res) => res.json({ "msg": "Welcome to the CK API" }));
+
+// Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));

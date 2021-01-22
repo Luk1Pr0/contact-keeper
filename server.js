20e425a868673c91de3fdac90ac789dd3db16b73
Luk1Pr0 <lpytel16@gmail.com>
2021-01-22 12:02:11 +0100

    User model created and regostering user works. Password is hashed with bcrypt

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
+// Connect database to the server
+connectDB();
+
+// Run server on given port
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 app.get('/', (req, res) => res.json({ "msg": "Welcome to the CK API" }));
@@ -14,5 +21,4 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-// Connect database to the server
-connectDB();
+

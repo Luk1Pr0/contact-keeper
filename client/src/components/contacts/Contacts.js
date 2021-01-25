3a431920d9fd0c071a98f23ac804039de32b7e39
Luk1Pr0 <lpytel16@gmail.com>
2021-01-25 15:39:55 +0100

    Contacts can now be filtered and the filtered contacts are displayed in the UI

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -5,13 +5,18 @@ import ContactItem from './ContactItem';
 const Contacts = () => {
 	const contactContext = useContext(ContactContext);
 
-	const { contacts } = contactContext;
+	const { contacts, filtered } = contactContext;
+
+	if (!contacts.length) {
+		return <h4 style={{ color: 'crimson'}}>Please add a new contact</h4>
+	}
 
 	return (
 		<div>
-			{contacts.map(contact => {
-				return <ContactItem key={contact.id} contact={contact} />
-			})
+			{filtered !== null ?
+				filtered.map(contact => <ContactItem key={contact.id} contact={contact} />)
+				:
+				contacts.map(contact => <ContactItem key={contact.id} contact={contact} />)
 			}
 		</div>
 	);

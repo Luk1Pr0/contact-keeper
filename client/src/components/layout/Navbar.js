e0d7359c5b059cc742c84a6d4032f6bf3562c374
Luk1Pr0 <lpytel16@gmail.com>
2021-02-08 12:39:23 +0100

    Created components for login and register pages

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,32 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Navbar = ({title, icon}) => {
-    return (
-        <div className='navbar bg-dark'>
-            <h1>
-                <i className={icon}></i> {title}
-            </h1>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-            </ul>
-        </div>
-    )
+const Navbar = ({ title, icon }) => {
+	return (
+		<div className='navbar bg-dark'>
+			<h1>
+				<i className={icon}></i> {title}
+			</h1>
+			<ul>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/about'>About</Link>
+				</li>
+				<li>
+					<Link to='/login'>Login</Link>
+				</li>
+				<li>
+					<Link to='/register'>Register</Link>
+				</li>
+			</ul>
+		</div>
+	)
 }
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string,
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.string,
 }
 
 Navbar.defaultProps = {
-    title: 'Contact Keeper',
-    icon: 'fas fa-id-card'
+	title: 'Contact Keeper',
+	icon: 'fas fa-id-card'
 }
 
 export default Navbar;

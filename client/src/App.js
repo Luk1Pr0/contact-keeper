449bac3b1b0dc4fcc07587c066f1a9ebcfe333dd
Luk1Pr0 <lpytel16@gmail.com>
2021-02-10 16:18:18 +0100

    Created an alert component for alerts

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
 const App = () => {
 	return (
@@ -20,6 +21,7 @@ const App = () => {
 						<div className="App">
 							<Navbar />
 							<div className="container">
+								<Alerts />
 								<Switch>
 									<Route exact path='/' component={Home} />
 									<Route exact path='/about' component={About} />
--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react';
 import { v4 as uuid } from 'uuid';
 import AlertContext from './alertContext';
-import authReducer from './authReducer';
+import alertReducer from './alertReducer';
 import { SET_ALERT, REMOVE_ALERT } from '../types';
 
 const AlertState = (props) => {
@@ -12,10 +12,7 @@ const AlertState = (props) => {
 	// Set Alert
 	const setAlert = (msg, type, timeout = 2000) => {
 		const id = uuid();
-		dispatch({
-			type: SET_ALERT,
-			payload: { msg, type, id },
-		});
+		dispatch({ type: SET_ALERT, payload: { msg, type, id } });
 
 		setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 	}
